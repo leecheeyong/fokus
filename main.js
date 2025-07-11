7549cbe7fda7eb0db4bbcab34ef644f75d21bae5
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2025-07-11 20:22:20 +0000

    fix

--- main.js
+++ main.js
@@ -192,41 +192,23 @@ function updateTimerButtons(elements) {
   elements.timerPause.disabled = !timerState.isRunning;
   if (timerState.isRunning) {
     elements.timerStart.innerHTML = `
-      <svg class="w-4 h-4 sm:w-5 sm:h-5 mr-2 inline" fill="currentColor" viewBox="0 0 20 20">
-        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd"></path>
-      </svg>
       Running
     `;
     elements.timerPause.innerHTML = `
-      <svg class="w-4 h-4 sm:w-5 sm:h-5 mr-2 inline" fill="currentColor" viewBox="0 0 20 20">
-        <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM7 8a1 1 0 012 0v4a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v4a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
-      </svg>
       Pause
     `;
   } else if (timerState.isPaused) {
     elements.timerStart.innerHTML = `
-      <svg class="w-4 h-4 sm:w-5 sm:h-5 mr-2 inline" fill="currentColor" viewBox="0 0 20 20">
-        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd"></path>
-      </svg>
       Resume
     `;
     elements.timerPause.innerHTML = `
-      <svg class="w-4 h-4 sm:w-5 sm:h-5 mr-2 inline" fill="currentColor" viewBox="0 0 20 20">
-        <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM7 8a1 1 0 012 0v4a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v4a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
-      </svg>
       Paused
     `;
   } else {
     elements.timerStart.innerHTML = `
-      <svg class="w-4 h-4 sm:w-5 sm:h-5 mr-2 inline" fill="currentColor" viewBox="0 0 20 20">
-        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd"></path>
-      </svg>
       Start
     `;
     elements.timerPause.innerHTML = `
-      <svg class="w-4 h-4 sm:w-5 sm:h-5 mr-2 inline" fill="currentColor" viewBox="0 0 20 20">
-        <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM7 8a1 1 0 012 0v4a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v4a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
-      </svg>
       Pause
     `;
   }
@@ -240,6 +222,7 @@ function updateProgressCircle(elements) {
   const circumference = 2 * Math.PI * 45;
   const offset = circumference - (progress * circumference);
   elements.progressCircle.style.strokeDashoffset = offset;
+  elements.progressCircle.setAttribute('stroke', timerState.isBreak ? 'url(#break-gradient)' : 'url(#gradient)');
 }
 
 function openSettings(elements) {
